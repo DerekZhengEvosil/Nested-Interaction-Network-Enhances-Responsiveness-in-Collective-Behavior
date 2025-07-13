2f687ead5d7da08e0163e49ff71163d00a76a426
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-07-13 23:17:45 +0800

    Update Figure4j.m

--- robotic_experimetns/Figure4j.m
+++ robotic_experimetns/Figure4j.m
@@ -1,3 +1,5 @@
+% This MATLAB code generates a violin plot to visualize the distribution of the average heading error in the collective following task. 
+% The plot will show how the heading error varies across different categories, with the mean value represented as a marker in the plot.
 clear;clc;
 addpath("../utility/")
 load("../data/robotic_experiments_data/collective_following_heading_error.mat")
