9faf5aae0df6384e50c37a3cf9dc578e28ae42cd
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-07-13 23:16:04 +0800

    Update Figure4e.m

--- robotic_experimetns/Figure4e.m
+++ robotic_experimetns/Figure4e.m
@@ -1,3 +1,5 @@
+% This MATLAB code generates a violin plot that visualizes the distribution of the mean curvature data collected from the robotic experiments. 
+% The plot shows how the curvature values vary across different categories, with each category represented by a violin plot.
 clear;clc;
 addpath("../utility/")
 load("../data/robotic_experiments_data/collective_hovering_curvature.mat")
