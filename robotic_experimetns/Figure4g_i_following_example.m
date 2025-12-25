9bbe97793ba45f0a484625689b17220b7236896c
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-25 22:18:22 +0800

    Update Figure4g_i_following_example.m

--- robotic_experimetns/Figure4g_i_following_example.m
+++ robotic_experimetns/Figure4g_i_following_example.m
@@ -1,6 +1,4 @@
-% This MATLAB code is used to visualize the robot trajectories during a following task under two different network models: 
-% PNIN (Potential Network Inference Network) and WSIN (Wavefront Sensing Inference Network). 
-% The code uses two separate data files, Following_PNIN.mat and Following_WSIN.mat, to visualize how robots behave under these two models.
+% This MATLAB code is used to visualize the robot trajectories during a following task under two different network models
 clear;clc
 addpath("../utility/")
 load("../data/robotic_experiments_data/Following_PNIN.mat")
