90c27e731d5bf23c7c24a483aa2826092f72d47b
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-25 21:58:42 +0800

    Update trajecotry_examples.m

--- collective_turns/trajecotry_examples.m
+++ collective_turns/trajecotry_examples.m
@@ -17,11 +17,8 @@ gen_acc_r_freq_patch(folder_name, 0, 1, " ", 1, 100, 2, -1.8);
 %% Figure 3h
 clear;clc
 addpath("../utility/")
-turning_angle = 75;
-standard_sig_file = "../data/collective_turns_data/standard_signal_ta=" + num2str(turning_angle) + ".mat"; % standard response signals
-load(standard_sig_file)
 % theta_info = 75
-load("../data/collective_turns_data/group_response_signals.mat")
+load("../data/collective_turns_data/Fig2h_data.mat")
 colorlist_1 = [254,229,217
 252,174,145
 251,106,74
@@ -30,26 +27,17 @@ colorlist_2 = [239,243,255
 189,215,231
 107,174,214
 33,113,181]./255;
-cal_ped_list = [50:10:100];
-mean_cos_heading_AMS = mean_cos_heading_AMS_cell{1};
-mean_cos_heading_NMS = mean_cos_heading_NMS_cell{1};
-mean_cos_heading_Ave = mean_cos_heading_Ave_cell{1};
-
-mean_cos_heading_AMS_tmp = mean_cos_heading_AMS{end}; % 1-none noise end-maximal noise
-mean_cos_heading_NMS_tmp = mean_cos_heading_NMS{end};
-mean_cos_heading_Ave_tmp = mean_cos_heading_Ave{end};
-
 figure
 figSize_L = 48;
 figSize_W = 5;
 set(gcf, 'Units', 'centimeter','Position', [5 5 figSize_L figSize_W])
-plot([1:500] * 0.2, mean_cos_heading_AMS_tmp(1:500,1), 'LineWidth',1,'Color',colorlist_1(end,:))
+plot(time_cell, mean_cos_heading_AMS_tmp_cell, 'LineWidth',1,'Color',colorlist_1(end,:))
 hold on
-plot([1:500] * 0.2, mean_cos_heading_NMS_tmp(1:500,1), 'LineWidth',1,'Color',colorlist_2(end,:))
+plot(time_cell, mean_cos_heading_NMS_tmp_cell, 'LineWidth',1,'Color',colorlist_2(end,:))
 hold on
-plot([1:500] * 0.2, mean_cos_heading_Ave_tmp(1:500,1), 'LineWidth',1,'Color',[127, 127, 127]./255)
+plot(time_cell, mean_cos_heading_Ave_tmp_cell, 'LineWidth',1,'Color',[127, 127, 127]./255)
 hold on
-plot([1:500] * 0.2, stand_cos_heading(1,1:500), '-k', 'LineWidth',0.5)
+plot(time_cell, stand_cos_heading_cell, '-k', 'LineWidth',0.5)
 xlim([0, 100])
 %         ylim([0, 1])
 xlabel("time [s]")
