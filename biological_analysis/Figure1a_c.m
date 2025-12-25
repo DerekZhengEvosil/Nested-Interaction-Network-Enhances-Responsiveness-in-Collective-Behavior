4ac8f56f0146f4ac4ae8a26a866182afd7a41c40
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-25 20:23:10 +0800

    Update Figure1a_c.m

--- biological_analysis/Figure1a_c.m
+++ biological_analysis/Figure1a_c.m
@@ -1,5 +1,5 @@
 clear;clc;
-addpath("../data/")
+addpath("../data/biological_data/")
 load("NODF_curvature_polarization")
 %% 
 % color_ind categorize the group size of fish data, 
@@ -12,40 +12,18 @@ figure
 figSize_L = 10;
 figSize_W = 10;
 set(gcf,'Units','centimeter','Position',[5 5 figSize_L figSize_W]);
-[rho,pval] = corr(draw_M_index',draw_op',"type","Spearman")
-g(1,1)=gramm('x',(draw_op));
-g(1,1).set_layout_options('Position',[0.1 0.8 0.62 0.2],... %Set the position in the figure (as in standard 'Position' axe property)
-    'legend',false,... % No need to display legend for side histograms
-    'margin_height',[0.02 0.05],... %We set custom margins, values must be coordinated between the different elements so that alignment is maintained
-    'margin_width',[0.1 0.02],...
-    'redraw',false); %We deactivate automatic redrawing/resizing so that the axes stay aligned according to the margin options
-g(1,1).set_names('x','');
-g(1,1).stat_bin('geom','stacked_bar','fill','transparent','normalization','probability'); %histogram
-g(1,1).axe_property('XTickLabel',''); % We deactivate tht ticks
-%Create a scatter plot
-g(2,1)=gramm('x',draw_op,'y',draw_M_index,'color',draw_op);
-g(2,1).set_names('x','average group polarization','y','average group curvature');
-g(2,1).geom_point(); %Scatter plot
-g(2,1).stat_glm();
-g(2,1).set_point_options('base_size',3);
-g(2,1).set_layout_options('position',[0.1 0.05 0.62 0.7],...
+g = gramm('x',draw_op,'y',draw_M_index,'color',draw_op);
+g.set_names('x','average group polarization','y','average group curvature');
+g.geom_point(); %Scatter plot
+g.stat_glm();
+g.set_point_options('base_size',3);
+g.set_layout_options('position',[0.1 0.05 0.8 0.8],...
     'legend_pos',[0.83 0.75 0.2 0.2],... %We detach the legend from the plot and move it to the top right
     'margin_height',[0.1 0.02],...
     'margin_width',[0.1 0.02],...
     'redraw',false);
-g(2,1).set_continuous_color('LCH_colormap',[20 80 ; 40 30 ; 260 260 ]);
-g(2,1).axe_property('box','on','XLim', [0.5,1]); 
-%Create y data histogram on the right
-g(3,1)=gramm('x',draw_M_index);
-g(3,1).set_layout_options('Position',[0.75 0.05 0.2 0.7],...
-    'legend',false,...
-    'margin_height',[0.1 0.02],...
-    'margin_width',[0.02 0.05],...
-    'redraw',false);
-g(3,1).set_names('x','');
-g(3,1).stat_bin('geom','stacked_bar','fill','transparent','normalization','probability'); %histogram
-g(3,1).coord_flip();
-g(3,1).axe_property('XTickLabel','');
+g.set_continuous_color('LCH_colormap',[20 80 ; 40 30 ; 260 260 ]);
+g.axe_property('box','on','XLim', [0.5,1]); 
 g.set_color_options('map','d3_10');
 g.draw();
 %% figure 1b
@@ -53,9 +31,6 @@ figure
 figSize_L = 10;
 figSize_W = 3.5;
 set(gcf,'Units','centimeter','Position',[5 5 figSize_L figSize_W]);
-% [b,~,~,~,stats] = regress(used_U_turn_NODF',used_op');
-[rho,pval] = corr(draw_U_turn_NODF',draw_op',"type","Spearman")
-%Create a scatter plot
 g = gramm('x',draw_op,'y',draw_U_turn_NODF,'color', color_ind);
 g.set_names('x','average group polarization','y','Nestedness');
 g.geom_point('alpha',0.7); %Scatter plot
@@ -73,8 +48,6 @@ figure
 figSize_L = 10;
 figSize_W = 3.5;
 set(gcf,'Units','centimeter','Position',[5 5 figSize_L figSize_W]);
-[rho,pval] = corr(draw_U_turn_NODF',draw_M_index',"type","Spearman")
-%Create a scatter plot
 g = gramm('x',draw_M_index,'y',draw_U_turn_NODF,'color', color_ind);
 g.set_names('x','average group curvature','y','Nestedness');
 g.geom_point('alpha',0.7); %Scatter plot
