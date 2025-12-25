cdc89f369b47a9cf4e141238fbe9405e84b81cb4
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-25 20:52:20 +0800

    Update effect_R0.m

--- SIR_simulation/effect_R0.m
+++ SIR_simulation/effect_R0.m
@@ -6,6 +6,7 @@ clear;clc;
 addpath("../data/SIR_data/")
 addpath("../utility/")
 load("../data/SIR_data/R0_data.mat")
+load("../data/SIR_data/R0_outdata.mat")
 figure
 figSize_L = 5;
 figSize_W = 4.5;
@@ -29,20 +30,16 @@ for muteRate = draw_mute_list
     mute_idx = find(muteRate == mute_list);
     ave_final_infected = fliplr(num_infected_ave_cell{mute_idx}');
     std_final_infected =  num_infected_std_cell{mute_idx};
-    high_CI_tmp = high_CI_cell{mute_idx};
-    low_CI_tmp = low_CI_cell{mute_idx};
     rec_scatter_tmp = rec_scatter_cell{mute_idx};
     num_infected_ave_scatter_tmp = num_infected_ave_scatter_cell{mute_idx};
     scatter(flip_R0(1:5:first_stop), ave_final_infected(1:5:first_stop), 8, 'MarkerFaceColor',mycolor(mute_idx,:),'MarkerEdgeColor','none','MarkerFaceAlpha', 0.3);
     hold on
     scatter(flip_R0(first_stop:stop_idx), ave_final_infected(first_stop:stop_idx), 8, 'MarkerFaceColor',mycolor(mute_idx,:),'MarkerEdgeColor','none','MarkerFaceAlpha', 0.3);
     hold on
-    smooth_tmp = smoothdata(ave_final_infected([1:1:stop_idx, stop_idx]),"gaussian");
-    e(mute_idx) = plot(flip_R0([1:1:stop_idx, stop_idx]), smooth_tmp, ...
+    e(mute_idx) = plot(flip_R0([1:1:stop_idx, stop_idx]), ave_final_infected_data{cnt} , ...
         'Color',mycolor(mute_idx,:),'LineWidth',2,'Marker','none','LineStyle','-','MarkerSize',10);
     hold on
-    R0_value(cnt) = flip_R0(find(smooth_tmp>th, 1));
-    fill([flip_R0, fliplr(flip_R0)], [fliplr(smooth(high_CI_tmp', 20)'), smooth(low_CI_tmp', 20)'], ...
+    fill([flip_R0, fliplr(flip_R0)], [fliplr(high_CI_tmp_cell{cnt}'), low_CI_tmp_cell{cnt}'], ...
         mycolor(mute_idx,:), 'FaceAlpha', 0.3, 'EdgeColor', 'none');
     hold on
     hold on
