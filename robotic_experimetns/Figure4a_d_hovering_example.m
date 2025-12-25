7aeef4e534b214b7c13136b7c1263ecec735b23a
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-25 22:17:27 +0800

    Update Figure4a_d_hovering_example.m

--- robotic_experimetns/Figure4a_d_hovering_example.m
+++ robotic_experimetns/Figure4a_d_hovering_example.m
@@ -1,6 +1,5 @@
 %% WSIN hovering basic info
-% This code involves two main tasks: loading data related to WSIN (Wavefront Sensing Inference Network) and PNIN (Potential Network Inference Network), 
-% then drawing and visualizing the robot trajectories during a collective hovering task in these two swarm models.
+% drawing and visualizing the robot trajectories during a collective hovering task in these two swarm models.
 clear;clc
 addpath("../utility/")
 dataFile = "../data/robotic_experiments_data/Hovering_WSIN.mat";
