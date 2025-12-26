ba7c0d25c10fea6cd751eb39477e1a6c39d52da3
DerekCheng <71299417+DerekZhengEvosil@users.noreply.github.com>
2025-12-26 10:32:00 +0800

    Update Figure4a_d_hovering_example.m

--- robotic_experimetns/Figure4a_d_hovering_example.m
+++ robotic_experimetns/Figure4a_d_hovering_example.m
@@ -4,10 +4,10 @@ clear;clc
 addpath("../utility/")
 dataFile = "../data/robotic_experiments_data/Hovering_WSIN.mat";
 load(dataFile)
-ck = drawRobotsTraj_hovering_info(G);
+drawRobotsTraj_hovering_info(G);
 %% PNIN hovering basic info
 clear;clc
 dataFile = "../data/robotic_experiments_data/Hovering_PNIN.mat";
 addpath("../utility/")
 load(dataFile)
-ck = drawRobotsTraj_hovering_info(G);
+drawRobotsTraj_hovering_info(G);
